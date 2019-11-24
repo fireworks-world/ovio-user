d8c7d64f020fa4a8314afbc40e2b443ceca8fc1e
Roboxo <Rishav.programmer@gmail.com>
2019-11-24 21:33:46 +0530

    Initial commit

---
+++ lib/slider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IntroSlider extends StatefulWidget {
+  IntroSlider({Key key}) : super(key: key);
+
+  @override
+  _IntroSliderState createState() => _IntroSliderState();
+}
+
+class _IntroSliderState extends State<IntroSlider> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Container(
+         
+       ),
+    );
+  }
+}
